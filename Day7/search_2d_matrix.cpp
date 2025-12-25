2b565e995d6c1ea320739412fb2abc32782fa872
prithviraj324 <rajchava342@gmail.com>
2025-12-26 00:52:47 +0530

    74. Search a 2D Matrix

--- Day7/search_2d_matrix.cpp
+++ Day7/search_2d_matrix.cpp
@@ -31,3 +31,29 @@ public:
 //----------------------------------------------------------------
 //solution might look like its O(logm * logn) but its actually not because:
 //there's only one case where target>col[first] AND target<col[last] hence its O(logm + logn)=O(log(mn))
+
+//Much more intuitive soln, treat matrix as a 1D sorted array and translate mid to 2D indices
+class Solution2 {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m = mat.size(), n = mat[0].size();
+
+        int linear_arr_idx = (m-1)*(n-1);
+        //row = idx/n, col = idx%n
+        int l = 0, r = m*n - 1;
+        
+        while(l<=r) {
+            int mid = (l+r) / 2;
+            int mid_r = mid/n, mid_c = mid%n;
+
+            if(target < mat[mid_r][mid_c]) 
+                r = (n*mid_r) + mid_c - 1;
+            if(target > mat[mid_r][mid_c]) 
+                l = (n*mid_r) + mid_c + 1;
+
+            if(mat[mid_r][mid_c] == target)
+                return true;
+        }
+        return false;
+    }
+};
